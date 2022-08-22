d9fcc6a5f2ca3edaf12900e83415d6b2228a6088
Mustafa Yasef Şahin <108290825+myasefsahin@users.noreply.github.com>
2022-08-22 14:24:14 +0300

    Delete example directory

--- example/example/src/main/java/com/proje/example/Repository/EmployeeRepo.java
+++
@@ -1,7 +0,0 @@
-package com.proje.example.Repository;
-
-import com.proje.example.Enitity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepo extends JpaRepository<Employee, Integer> {
-}
