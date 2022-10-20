0f3da3b573829e7217d8e694312f27f55d770788
Mustafa Yasef Şahin <108290825+myasefsahin@users.noreply.github.com>
2022-10-20 18:21:08 +0300

    Add files via upload

---
+++ Atm Uygulaması Memory/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Islem islem=new Islem();
+        while (true){
+            islem.giris();
+        }
+
+    }
+
+
+
+
+}
